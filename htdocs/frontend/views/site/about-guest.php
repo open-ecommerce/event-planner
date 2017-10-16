1758b5451ece2fef04e918a693b3c984973086a3
Eduardo Silva <eduardo@open-ecommerce.org>
2017-10-16 14:18:41 +0100

    about us

--- htdocs/frontend/views/site/about-guest.php
+++ htdocs/frontend/views/site/about-guest.php
@@ -6,7 +6,24 @@ use \common\widgets\DbText;
 
 $this->title = 'About us';
 ?>
-<div class="site-about">
-    <h1>About us</h1>
-    <?= DbText::widget(['key' => 'about-us-guest']); ?>
+<div class="site-about clients">
+    <div class="container about">
+        <div class="row">
+            <div class="col-xm-12 col-md-12">
+                <div id="about-main-wraper">
+                    <?php if (Yii::$app->user->can("administrator")): ?>                
+                        <div class="row">                   
+                            <div class="text-right">
+                                <?=
+                                Html::a('<i class="glyphicon glyphicon-pencil"></i>', Yii::$app->urlManager->createUrl(array('/admin/widget-text/update?id=4')), ['class' => 'btn btn-success', 'target' => '_blank']);
+                                ?>
+                            </div>
+                        </div>
+                    <?php endif; ?>
+                    <?= DbText::widget(['key' => 'about-us-main']); ?>
+                </div>
+            </div>
+        </div>
+    </div>    
 </div>
+
